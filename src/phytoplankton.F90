af0f112cf6d41ad39a10bec5b14e4ade053c0018
Nicolas Azana <nicoal@bolding-bruggeman.com>
2024-05-06 15:08:48 +0200

    Further implementation of UVic npzd code, as well as implementation of geochemical tracers - both in the npzd modules, and in a new nut_chem.f90 module that handles these centrally.
    The code base still lacks an implementation of the UVic sediment model.

--- src/phytoplankton.F90
+++ src/phytoplankton.F90
@@ -11,13 +11,14 @@ module uvic_phytoplankton
    private
 
    type, extends(type_particle_model), public :: type_uvic_phytoplankton
-      type (type_state_variable_id)     :: id_phyt, id_phosphorus, id_no3, id_det
+      type (type_state_variable_id)     :: id_phyt, id_phosphorus, id_no3, id_det, id_o2, id_alk
       type (type_surface_dependency_id) :: id_fe, id_dayfrac
       type (type_dependency_id)         :: id_temp, id_depth, id_sw_par, id_f1
       type (type_global_dependency_id)  :: id_latitude
+      type (type_diagnostic_variable_id):: id_morp_out, id_npp_out, id_morpt_out, id_avej_out, id_no3P_out, id_po4P_out
       
-      real(rk)                            :: abio, bbio, cbio, kfe, alpha, k1n, nup, nupt0
-      logical                             :: no_temp_sens, nitrogen, no3_sens, fe_limitation, so_fe_fer, cdom_attenuation
+      real(rk)                            :: abio, bbio, cbio, kfe, alpha, k1n, nup, nupt0, jdiar
+      logical                             :: no_temp_sens, nitrogen, no3_sens, fe_limitation, so_fe_fer, cdom_attenuation, extra_diags
    contains
       procedure :: initialize
       procedure :: do
@@ -30,26 +31,30 @@ contains
       integer,                    intent(in)            :: configunit
 
       ! parameters
-      call self%get_parameter(self%alpha,        'alpha',        '(W/m2)-1 d-1','Initial slope P-I curve',                     default=0.1_rk, scale_factor=d_per_s)
-      call self%get_parameter(self%k1n,          'k1n',          'mmol N m-3',  'Half saturation constant for N uptake',       default=0.7_rk)
-      call self%get_parameter(self%nup,          'nup',          'd-1',         'Specific mortality rate',                     default=0.025_rk)
-      call self%get_parameter(self%nupt0,        'nupt0',        'd-1',         'temp. dependent specific mortality rate',     default=0.02_rk)
-      call self%get_parameter(self%nitrogen,     'nitrogen',     '',            'turn on nitrogen dependency',                 default=.true.)
-      !if (self%nitrogen) then                                                   
-      !    call self%get_parameter(self%no3_sens, 'n03_sens',     '',            'turn on remineralization nitrate dependence', default=.true.)
-      !endif                                                                     
-      call self%get_parameter(self%abio,         'abio',         'd-1',         'maximum growth rate',                         default=0.18_rk, scale_factor=d_per_s)
-      call self%get_parameter(self%no_temp_sens, 'no_temp_sens', '',            'turn off temperature sensitivity',            default=.false.)                                                                                                                               
-      if (.not. self%no_temp_sens) then                                         
-          call self%get_parameter(self%bbio,     'bbio',         '-',           'temperature dependency intercept',            default=1.066_rk)
-          call self%get_parameter(self%cbio,     'cbio',         'degC-1',      'temperature dependency exponent',             default=1.0_rk)
+      call self%get_parameter(self%alpha,        'alpha',        '(W/m2)-1 d-1','Initial slope P-I curve',                        default=0.1_rk, scale_factor=d_per_s)
+      call self%get_parameter(self%k1n,          'k1n',          'mmol N m-3',  'Half saturation constant for N uptake',          default=0.7_rk)
+      call self%get_parameter(self%nup,          'nup',          'd-1',         'Specific mortality rate',                        default=0.025_rk)
+      call self%get_parameter(self%nupt0,        'nupt0',        'd-1',         'temp. dependent specific mortality rate',        default=0.02_rk)
+      call self%get_parameter(self%nitrogen,     'nitrogen',     '',            'turn on nitrogen dependency',                    default=.true.)
+      if (self%nitrogen) then                                                   
+          call self%get_parameter(self%nfix,     'nfix',         '',            'turn on n-fixation',                             default=.false.)
+          if (self%nfix) then                                                   
+              call self%get_parameter(self%jdiar,'jdiar',        '',            'factor reducing the growth rate of diazotrophs', default=0.5_rk)
+          endif
       endif                                                                     
-      call self%get_parameter(self%fe_limitation,'fe_limitation','',            'turn on iron limitation',                     default=.true.)                                                                                                                               
-      if (self%fe_limitation) then                                              
-          call self%get_parameter(self%kfe,      'kfe',          'mmol Fe m-3', 'iron half saturation constant',               default=0.1_rk)
-          call self%get_parameter(self%so_fe_fer,'so_fe_fer',    '',            'iron fertilization in the southern ocean',    default=.false.)
-      endif
-      call self%get_parameter(self%cdom_attenuation,'cdom_attenuation','',      'cdom_attenuation',                            default=.false.)
+      call self%get_parameter(self%extra_diags,  'extra_diags',  '',            'turn on extra diagnostic variables',             default=.false.)    
+      call self%get_parameter(self%abio,         'abio',         'd-1',         'maximum growth rate',                            default=0.18_rk, scale_factor=d_per_s)
+      call self%get_parameter(self%no_temp_sens, 'no_temp_sens', '',            'turn off temperature sensitivity',               default=.false.)                                                                                                                               
+      if (.not. self%no_temp_sens) then                                                                                           
+          call self%get_parameter(self%bbio,     'bbio',         '-',           'temperature dependency intercept',               default=1.066_rk)
+          call self%get_parameter(self%cbio,     'cbio',         'degC-1',      'temperature dependency exponent',                default=1.0_rk)
+      endif                                                                                                                       
+      call self%get_parameter(self%fe_limitation,'fe_limitation','',            'turn on iron limitation',                        default=.true.)                                                                                                                               
+      if (self%fe_limitation) then                                                                                                
+          call self%get_parameter(self%kfe,      'kfe',          'mmol Fe m-3', 'iron half saturation constant',                  default=0.1_rk)
+          call self%get_parameter(self%so_fe_fer,'so_fe_fer',    '',            'iron fertilization in the southern ocean',       default=.false.)
+      endif                                                                                                                       
+      call self%get_parameter(self%cdom_attenuation,'cdom_attenuation','',      'cdom_attenuation',                               default=.false.)
       
       
       ! variable registrations
@@ -59,20 +64,35 @@ contains
       call self%add_to_aggregate_variable(type_interior_standard_variable(name='total_phytoplankton',units='mmol N m-3'), self%id_phyt)
       
       ! environmental dependencies
-      call self%register_dependency(self%id_sw_par, 'sw_par', 'W m-2', 'average layer PAR')
-      call self%register_dependency(self%id_f1,     'f1',     '-',     'current layer light extinction coeff.')
-      call self%register_dependency(self%id_dayfrac,'dayfrac','-',     'daylength fraction')
-      call self%register_state_dependency(self%id_phosphorus, 'p',     'mmol p m-3',  'dissolved inorganic phosphorous concentration')
-      call self%register_state_dependency(self%id_det,        'detritus', 'mmol N m-3',  'detritus concentration')
-      if (self%nitrogen) then
-          call self%register_state_dependency(self%id_no3, 'no3',     'mmol n m-3',  'dissolved inorganic nitrogen concentration')
-      endif
-      if (self%fe_limitation) then
-          call self%register_surface_dependency(self%id_fe, 'fe',     'mmol Fe m-3',  'photic zone iron concentration')
+      call self%register_dependency(self%id_sw_par,           'sw_par',           'W m-2',       'average layer PAR')
+      call self%register_dependency(self%id_f1,               'f1',               '-',           'current layer light extinction coeff.')
+      call self%register_dependency(self%id_dayfrac,          'dayfrac',          '-',           'daylength fraction')
+      call self%register_state_dependency(self%id_phosphorus, 'p',                'mmol p m-3',  'dissolved inorganic phosphorous concentration')
+      call self%register_state_dependency(self%id_det,        'detritus',         'mmol N m-3',  'detritus concentration')
+      call self%register_state_dependency(self%id_o2,         'oxygen',           'umol cm-3',   'oxygen concentration')
+      call self%register_state_dependency(self%id_alk,        'alkalinity',       'umol cm-3',   'ocean alkalinity')
+      call self%register_state_dependency(self%id_calc,       'calcite detritus', 'umol C m-3',  'detrital calcite concentration')
+      if (self%nitrogen) then                                                            
+          call self%register_state_dependency(self%id_no3,    'no3',      'mmol n m-3',  'dissolved inorganic nitrogen concentration')
+      endif                                                                              
+      if (self%fe_limitation) then                                                       
+          call self%register_surface_dependency(self%id_fe,   'fe',       'mmol Fe m-3', 'photic zone iron concentration')
       endif
       call self%register_dependency(self%id_temp,      standard_variables%temperature)
       call self%register_dependency(self%id_depth,     standard_variables%depth)
       call self%register_dependency(self%id_latitude,  standard_variables%latitude)
+      
+      ! register diagnostic variables for output
+      call self%register_diagnostic_variable(self%id_morp_out,  'morp_out',  'mmol m-3 s-1', 'quadratic mortality of phytoplankton')
+      call self%register_diagnostic_variable(self%id_npp_out,   'npp_out',   'mmol m-3 s-1', 'net primary production')
+      call self%register_diagnostic_variable(self%id_morpt_out, 'morpt_out', 'mmol m-3 s-1', 'specific mortality of phytoplankton')
+      if (self%extra_diags) then
+          call self%register_diagnostic_variable(self%id_avej_out, 'avej_out', 's-1', 'light-depend phyt. growth rate')
+          call self%register_diagnostic_variable(self%id_no3P_out, 'no3P_out', 's-1', 'no3 depend. phyt growth rate')
+          call self%register_diagnostic_variable(self%id_po4P_out, 'po4P_out', 's-1', 'po4 depend. phyt growth rate')
+      endif
+      
+          
       
    end subroutine initialize
    
@@ -80,7 +100,7 @@ contains
       class (type_uvic_phytoplankton), intent(in) :: self
       _DECLARE_ARGUMENTS_DO_
       
-      real(rk) :: depth, lat, temp, felimit, fe_conc, jmax, sw_par, gl, f1, gd, u1, u2, phi1, phi2, dayfrac, p, cdom_factor, avej, k1p, u_P, po4P, no3, no3P, npp, phyt, temp
+      real(rk) :: depth, lat, temp, felimit, fe_conc, jmax, sw_par, gl, f1, gd, u1, u2, phi1, phi2, dayfrac, p, cdom_factor, avej, k1p, u_P, po4P, no3, no3P, npp, phyt, temp, no3upt, o2, Paulmier_a, Paulmier_z, Paulmier_R0
       
       _LOOP_BEGIN_
          _GET_(self%id_sw_par, sw_par)
@@ -89,6 +109,9 @@ contains
          _GET_(self%id_phosphorus, p)
          _GET_(self%id_phyt, phyt)
          _GET_(self%id_temp, temp)
+         _GET_(self%id_o2, o2)
+         pflag = 0.5_rk + sign(0.5_rk,phyt - trcmin)
+         nflag = 0.5_rk + sign(0.5_rk,p - trcmin)
          
          gl = 2._rk*self%alpha*sw_par
          
@@ -106,18 +129,28 @@ contains
              felimit = 1.0_rk
          endif
          
+         jdiar = 1.0_rk
+         
          ! temperature- and irondependent max growth rate:
          if (self%no_temp_sens) then
-             jmax = self%abio*3.84_rk*felimit
+             if (self%nitrogen) then
+                 jmax = max(0.0_rk,self%abio*2.72_rk*felimit)*self%jdiar
+             else
+                 jmax = self%abio*3.84_rk*felimit
+             endif
              nupt = self%nupt0*4.01_rk
          else
              _GET_(self%id_temp, temp)
              bct = self%bbio**(self%cbio*temp)
-             jmax = self%abio*bct*felimit
+             if (self%nitrogen .and. self%nfix) then
+                 jmax = max(0.0_rk,self%abio*(bct-2.6_rk)*felimit)*self%jdiar
+             else
+                 jmax = self%abio*bct*felimit
+             endif
              nupt = self%nupt0*bct
          endif
          
-         gd = jmax*dayfrac
+         gd = max(1.0e-14_rk,jmax*dayfrac)
          u1 = max(gl/gd,1.e-6_rk)
          u2 = u1*exp(f1*dzt)
          ! for the following approximation ensure that u1 < 20
@@ -132,24 +165,51 @@ contains
          avej = gd*(phi1 - phi2)/(cdom_factor*abs(f1)*dzt)
          
          ! mortality
-         morp = self%nup*phyt
-         morpt = nupt*phyt
+         morp = self%nup*phyt * pflag ! if (self%nitrogen) morp = 0.0_rk
+         morpt = nupt*phyt * pflag
 
-         
          k1p = self%k1n*redptn
          po4P = jmax*p/(k1p + p) 
          u_P = min(avej, po4P)
+         no3flag = 1.0_rk
          if (self%nitrogen) then
              _GET_(self%id_no3, no3)
-             u_P = min(u_P, jmax*no3/(self%k1n + no3))
-             no3P = jmax*no3/(self%k1n + no3)
-             _ADD_SOURCE_(self%id_no3, morpt-u_P*phyt)
+             no3flag = 0.5_rk + sign(0.5_rk,no3 - trcmin)
+             if (self%nfix) then
+                 morp = 0.0_rk
+                 npp = u_P*phyt * no3flag*nflag
+                 no3upt = no3/(self%k1n + no3)*npp * no3flag*nflag ! nitrate uptake
+                 o2_roc =    (npp - no3upt)*1.25e-3_rk    ! check units of oxygen tracer
+                 alk_roc = - (npp - no3upt)*1e-3_rk
+                 
+                 _ADD_SOURCE_(self%id_o2,  o2_roc)
+                 _ADD_SOURCE_(self%id_alk, alk_roc)
+                 _ADD_SOURCE_(self%id_no3, morpt-no3upt)
+             else
+                 u_P = min(u_P, jmax*no3/(self%k1n + no3))
+                 npp = u_P*phyt * no3flag*nflag
+                 no3P = jmax*no3/(self%k1n + no3)
+                 _ADD_SOURCE_(self%id_no3, morpt-npp)
+             endif             
          endif
-         npp = u_P*phyt
          
+         ! update state variables
          _ADD_SOURCE_(self%id_phosphorus, (morpt-npp)*redptn)
          _ADD_SOURCE_(self%id_phyt,        npp-morpt-morp)
          _ADD_SOURCE_(self%id_det,         morp)
+         _ADD_SOURCE_(self%id_calc,        morp*self%capr*redctn)
+         
+         ! diagnostic output
+         _SET_DIAGNOSTIC_(self%id_morp_out, morp)
+         _SET_DIAGNOSTIC_(self%id_npp_out, npp)
+         _SET_DIAGNOSTIC_(self%id_morpt_out, morpt)
+         if (self%extra_diags) then
+             _SET_DIAGNOSTIC_(self%id_avej_out, avej)
+             _SET_DIAGNOSTIC_(self%id_po4P_out, po4P)
+             if (self%nitrogen) then
+                 _SET_DIAGNOSTIC_(self%id_no3P_out, no3P)
+             endif
+         endif
       _LOOP_END_
    end subroutine do
 
--- src/zooplankton.F90
+++ src/zooplankton.F90
@@ -11,16 +11,18 @@ module uvic_zooplankton
    private
 
    type, extends(type_particle_model), public :: type_uvic_zooplankton
-      type (type_state_variable_id)                            :: id_zoop, id_o2, id_phosphorus, id_no3, id_det
-      type (type_dependency_id)                                :: id_temp
-      type (type_model_id),          allocatable, dimension(:) :: id_prey
-      type (type_state_variable_id), allocatable, dimension(:) :: id_pbio
-
+      type (type_state_variable_id)                                 :: id_zoop, id_o2, id_phosphorus, id_no3, id_det, id_calc
+      type (type_dependency_id)                                     :: id_temp
+      type (type_model_id),          allocatable, dimension(:)      :: id_prey
+      type (type_state_variable_id), allocatable, dimension(:)      :: id_pbio
+      type (type_diagnostic_variable_id)                            :: id_graz_out, id_morz_out, id_excr_out, id_gmax_out
+      type (type_diagnostic_variable_id), allocatable, dimension(:) :: id_graz_i_out                
       
-      real(rk)                            :: gbio, bbio, cbio, nu, gamma1, ge
+      
+      real(rk)                            :: gbio, bbio, cbio, nu, gamma1, geZ
       real(rk), allocatable, dimension(:) :: zpref
       integer                             :: nprey
-      logical                             :: no_temp_sens, graz_upper_temp_limit, nexcr 
+      logical                             :: no_temp_sens, graz_upper_temp_limit, nitrogen, extra_diags
    contains
       procedure :: initialize
       procedure :: do
@@ -33,19 +35,20 @@ contains
       integer,                    intent(in)            :: configunit
 
       ! parameters
-      call self%get_parameter(self%no_temp_sens,          'no_temp_sens',          '',                    'turn off temperature sensitivity',  default=.false.)
+      call self%get_parameter(self%extra_diags,           'extra_diags',           '',                    'turn on extra diagnostic variables',  default=.false.)
+      call self%get_parameter(self%no_temp_sens,          'no_temp_sens',          '',                    'turn off temperature sensitivity',    default=.false.)
       if (.not. self%no_temp_sens) then
-          call self%get_parameter(self%bbio,                  'bbio',                  '-',                   'upper limit grazing intercept',     default=1.066_rk)
-          call self%get_parameter(self%cbio,                  'cbio',                  'degC-1',              'upper limit grazing exponent',      default=1.0_rk)
-      endif
-      call self%get_parameter(self%graz_upper_temp_limit, 'graz_upper_temp_limit', '',                    'turn on upper temperature limit',   default=.true.)
-      call self%get_parameter(self%nexcr,                 'nexcr',                 '',                    'turn on nitrogen excretion',        default=.false.)
-                                                                                                                                               
-      call self%get_parameter(self%gbio,                  'gbio',                  'd-1',                 'Maximum grazing rate at 0 deg C',   default=0.18_rk, scale_factor=d_per_s)
-      call self%get_parameter(self%nprey,                 'nprey',                 '-',                   'number of prey types',              default=1)
-      call self%get_parameter(self%nu,                    'nu',                    '(mmol m-3)^(-2) d-1', 'quadratic mortality',               default=0.34_rk, scale_factor=d_per_s)
-      call self%get_parameter(self%gamma1,                'gamma1',                '-',                   'assimilation efficiency',           default=0.7_rk)
-      call self%get_parameter(self%ge,                    'ge',                    '-',                   'growth efficiency',                 default=0.5_rk)
+          call self%get_parameter(self%bbio,              'bbio',                  '-',                   'upper limit grazing intercept',       default=1.066_rk)
+          call self%get_parameter(self%cbio,              'cbio',                  'degC-1',              'upper limit grazing exponent',        default=1.0_rk)
+      endif                                                                                                                                     
+      call self%get_parameter(self%graz_upper_temp_limit, 'graz_upper_temp_limit', '',                    'turn on upper temperature limit',     default=.true.)
+      call self%get_parameter(self%nitrogen,              'nitrogen',              '',                    'turn on nitrogen excretion',          default=.false.)
+                                                                                                                                                 
+      call self%get_parameter(self%gbio,                  'gbio',                  'd-1',                 'Maximum grazing rate at 0 deg C',     default=0.18_rk, scale_factor=d_per_s)
+      call self%get_parameter(self%nprey,                 'nprey',                 '-',                   'number of prey types',                default=1)
+      call self%get_parameter(self%nu,                    'nu',                    '(mmol m-3)^(-2) d-1', 'quadratic mortality',                 default=0.34_rk, scale_factor=d_per_s)
+      call self%get_parameter(self%gamma1,                'gamma1',                '-',                   'assimilation efficiency',             default=0.7_rk)
+      call self%get_parameter(self%geZ,                   'geZ',                   '-',                   'growth efficiency',                   default=0.5_rk)
       
       ! prey coupling
       allocate(self%zpref(self%nprey))
@@ -67,11 +70,25 @@ contains
       
       ! environmental dependencies
       call self%register_dependency(self%id_temp,      standard_variables%temperature)
-      call self%register_state_dependency(self%id_o2,         'o2',       'mmol O m-3',  'oxygen concentration')
-      call self%register_state_dependency(self%id_det,        'detritus', 'mmol N m-3',  'detritus concentration')
-      call self%register_state_dependency(self%id_phosphorus, 'p',        'mmol P m-3',  'dissolved inorganic phosphorous concentration')
-      if (self%nexcr) then
+      call self%register_state_dependency(self%id_o2,         'o2',               'umol O cm-3', 'oxygen concentration')
+      call self%register_state_dependency(self%id_det,        'detritus',         'mmol N m-3',  'detritus concentration')
+      call self%register_state_dependency(self%id_calc,       'calcite detritus', 'umol C cm-3',  'detrital calcite concentration')
+      call self%register_state_dependency(self%id_phosphorus, 'p',                'mmol P m-3',  'dissolved inorganic phosphorous concentration')
+      if (self%nitrogen) then
           call self%register_state_dependency(self%id_no3, 'n03',     'mmol N m-3',  'dissolved inorganic nitrogen concentration')
+      endif
+
+      ! register diagnostic variables for output
+      call self%register_diagnostic_variable(self%id_graz_out, 'grazing',   'mmol m-3 s-1', 'total grazing rate')
+      call self%register_diagnostic_variable(self%id_morz_out, 'mortality', 'mmol m-3 s-1', 'mortality rate')
+      call self%register_diagnostic_variable(self%id_excr_out, 'excretion', 'mmol m-3 s-1', 'excretion rate')
+      if (self%extra_diags) then
+          call self%register_diagnostic_variable(self%id_gmax_out, 'max_grazing',   'mmol m-3 s-1', 'maximum grazing rate')
+          allocate(self%id_graz_i_out(self%nprey))
+          do iprey = 1, self%nprey
+              write (index, '(i0)') iprey
+              call self%register_diagnostic_variable(self%id_graz_i_out(iprey), 'grazing'//trim(index),   'mmol m-3 s-1', 'total grazing rate on prey '//trim(index))
+          enddo
       endif
    end subroutine initialize
    
@@ -80,7 +97,7 @@ contains
       _DECLARE_ARGUMENTS_DO_
       
       real(rk)                           :: bctz, zoop, o2, temp, gmax, thetaZ, excr, prey_s, zoop_roc
-      real(rk),   dimension(self%nPrey)  :: pbio, ing, gs, graz
+      real(rk),   dimension(self%nPrey)  :: pbio, ing, gs, graz, pflag
       
       _LOOP_BEGIN_
          _GET_(self%id_zoop, zoop)
@@ -88,6 +105,7 @@ contains
          _GET_(self%id_temp, temp)
          do iprey=1,self%nprey
              _GET_(self%id_pbio(iprey),pbio(iprey))
+             pflag(iprey) = 0.5_rk + sign(0.5_rk,pbio(iprey) - trcmin)
          enddo
          
          if (no_temp_sens) then
@@ -102,38 +120,50 @@ contains
          ! Make the max grazing rate a function of temperature
          gmax = self%gbio*bctz
          
+         zflag = 0.5_rk + sign(0.5_rk,zoop - trcmin)
          if (sum(self%zpref) - 1._rk > 1.e-15_rk) then
              call self%fatal_error('zooplankton', 'prey preferences do not sum up to one')         
          endif
          thetaZ = sum(self%zpref*pbio) + kzoo
          ing = self%zpref/thetaZ
-         gs = 
-         graz = gmax*ing*pbio*zoop
+         graz = gmax*ing*pbio*zoop*pflag*zflag
          
-         mor = self%nu*zoop*zoop
+         mor = self%nu*zoop*zoop*zflag
          
          excr = (self%gamma1-self%geZ)*sum(graz)
          
          zoop_roc = self%geZ*sum(graz) - mor
          det_roc = (1._rk-self%gamma1)*sum(graz) + mor
-         
+         calc_roc = det_roc*self%capr*redctn
+                  
          !update state variable sources/sinks
          _ADD_SOURCE_(self%id_zoop, zoop_roc)
          
          do iprey = 1, self%nprey
              do istate = 1, size(self%id_prey(iprey)%state
                  _GET_(self%id_prey(iprey)%state(istate), prey_s)
-                 _ADD_SOURCE_(self%id_prey(iprey)%state(istate), - gs(iprey)*prey_s)
+                 _ADD_SOURCE_(self%id_prey(iprey)%state(istate), - graz(iprey)*prey_s)
              enddo
          enddo
-                 
          _ADD_SOURCE_(self%id_det, det_roc)
+         _ADD_SOURCE_(self%id_calc, calc_roc)
          
-         _ADD_SOURCE_(self%id_phosphorus, redptn*excr)
-         if (self%nexcr) then
+         _ADD_SOURCE_(self%id_phosphorus,  redptn*excr)
+         _ADD_SOURCE_(self%id_o2,         -excr*redptn*redotp)
+         if (self%nitrogen) then
              _ADD_SOURCE_(self%id_no3, excr)
-         endif  
+         endif
+         
+         ! diagnostic output
+         _SET_DIAGNOSTIC_(self%id_graz_out, sum(graz))
+         _SET_DIAGNOSTIC_(self%id_morz_out, mor)
+         _SET_DIAGNOSTIC_(self%id_excr_out, excr)
+         if (self%extra_diags) then
+             _SET_DIAGNOSTIC_(self%id_gmax_out, gmax)
+             do iprey = 1, self%nprey
+                 _SET_DIAGNOSTIC_(self%id_graz_i_out(iprey), graz(iprey))
+             enddo
+         endif
       _LOOP_END_
    end subroutine do
-
 end module
